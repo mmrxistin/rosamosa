bf71eb8d90538f3503b8bcef1c3de83d7e82a24f
mmrxistin <mmrxistin@gmail.com>
2026-07-31 10:35:46 +0300

    Suphan Allah ul Azim ve Bihamdihi

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -7,7 +7,7 @@
 // Bila Allah Azze ve Celle me ji sunneta Resulullah Muhammed (s.a.v) neqetine, amin rabbal alemin 
 // Xeyni Allah tu Xweda tune,Allah yeke û tu şirîkê Allah tune, pesin de jî mulk de jî û spasî tenê Allah re tê kirin.
 // Hz Mıhemed ji qûl u resûlê wi ye 
-
+// Elhamdulillah Ya Kerim, Ya Rahim, Ya Samed, Ya Vehhab, Ya Quddus, Ya Erhamer Rahimin, Ya Gafur, Ya Rahim, Ya Halim, Ya Azim, Ya Aziz, Ya Hakim, Ya Latif, Ya Hafiz, Ya Kadir, Ya Muheymin, Ya Ehed, Ya Alim, Ya Basir, Ya Sami, Ya Halim, Ya Gani
 
 import { validateRequest } from "@/auth";
 import { redirect } from "next/navigation";
