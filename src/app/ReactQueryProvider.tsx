398f0e73512c2750a3803b4bd2db55985453be74
mmrxistin <mmrxistin@gmail.com>
2026-08-26 12:44:55 +0300

    Suphan Allah î Ehed î Kerîm î Xaliq î Rezzaq î Quddus î Haq î Vehhab ve Bîhamdîhî VelHamdu Lîllah

--- src/app/ReactQueryProvider.tsx
+++ src/app/ReactQueryProvider.tsx
@@ -1,3 +1,15 @@
+// Bismillahirrahmanirrahim
+// Elhamdulillahirabbulalemin
+// Esselatu vesselamu ala rasulillah
+// Allahumme salli ala seyyidina Muhammedin
+// Allah u Ekber, Allahu Ekber, Allahu Ekber
+// La ilahe illAllah, Allahu Ekber,
+// SubhanAllah, Elhamdulillah, Allahu Ekber
+// Estaxfurullah El Azim
+// Allahu Ekber ve Lillahil Hamd
+
+
+
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -18,3 +30,16 @@ export default function ReactQueryProvider({
     </QueryClientProvider>
   );
 }
+// Elhamdulillah Elhamdulillah Elhamdulillah
+// Elhamdulillahirabbilalemin
+// La îlahe îll Allah û vahdehû(Esma ul Husna) la şerîke leh, lehul-mülkü ve lehul-hamdü .
+
+// Elhamdulillah Elhamdulillah Elhamdulillah
+// Elhamdulillahirabbilalemin
+// La îlahe îll Allah û vahdehû(Esma ul Husna) la şerîke leh, lehul-mülkü ve lehul-hamdü ,
+
+
+// Yuhyî ve yumît
+// Bîyadîhîl xayr
+// ve hüve alâ külli şeyin kadîr
+// ALLAH U EKBER VELİLLAHIL HAMD
